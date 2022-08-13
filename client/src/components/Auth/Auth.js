fe9e3895c79f665864cc5b6f2850128429552a29
Osama <osamazahid689@gmail.com>
2022-08-13 05:17:27 +0500

    first commit

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import { useNavigate  } from "react-router-dom"
+import { GoogleLogin } from 'react-google-login';
+import {gapi} from 'gapi-script'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import {Google} from 'react-google-login'
+import Icon from './icon';
+import { signin, signup } from '../../actions/auth';
+import { AUTH } from '../../constants/actionTypes';
+import useStyles from './styles';
+import Input from './Input';
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
+const clientId="955942897457-ajh5lgtf34t89vlk90o1ra262ict56j4.apps.googleusercontent.com"
+const SignUp = () => {
+  const [form, setForm] = useState(initialState);
+  const [isSignup, setIsSignup] = useState(false);
+  const dispatch = useDispatch();
+  const history = useNavigate ();
+  const classes = useStyles();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () => setShowPassword(!showPassword);
+
+  const switchMode = () => {
+    setForm(initialState);
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    setShowPassword(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isSignup) {
+      dispatch(signup(form, history));
+    } else {
+      dispatch(signin(form, history));
+    }
+  };
+
+  const onSuccess = (res)=>{
+    console.log("Login Success! Current user: ", res.profileObj);
+  }
+
+  const onFailure = (res) =>{
+    console.log("Login failed! res: ", res)
+  }
+
+  useEffect(()=>{
+    function start(){
+      gapi.auth2.init({
+        clientId: clientId,
+        scope: ""
+      })
+    };
+
+    gapi.load('client:auth2', start)
+  })
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">{ isSignup ? 'Sign up' : 'Sign in' }</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            { isSignup && (
+            <>
+              <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+              <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+            </>
+            )}
+            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+            <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+            { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            { isSignup ? 'Sign Up' : 'Sign In' }
+          </Button>
+
+          {/* Google Sign in */}
+          <GoogleLogin
+            clientId = {clientId}
+            buttonText="Sign in with Google"
+            onSuccess={onSuccess}
+            onFailure= {onFailure}
+            cookiePolicy={'signle_host_origin'}
+            isSignedIn = {true}
+            
+          />
+          {/* Google Sign in */}
+
+
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default SignUp;
